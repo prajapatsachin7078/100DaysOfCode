cb7943494c4dbbabc04d0f664f62eb1cb9679611
Sachin <prajapatsachin7078@gmail.com>
2023-03-31 21:36:25 +0530

    multiply.java file created

---
+++ multiply.java
@@ -0,0 +1,7 @@
+public class multiply {
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 4;
+        System.out.println("Multiplication of " + a + " and " + b + " " + a*b);
+    }
+}
