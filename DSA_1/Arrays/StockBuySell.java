96c7b7494d0c2105dcf83a4b2f90591ee05db35d
Sachin <prajapatsachin7078@gmail.com>
2023-11-23 13:02:49 +0530

    1Day completed

--- DSA_1/Arrays/StockBuySell.java
+++ DSA_1/Arrays/StockBuySell.java
@@ -1,8 +1,31 @@
 package DSA_1.Arrays;
 
+import java.util.Scanner;
+
 /**
  * StockBuySell
  */
 public class StockBuySell {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number of Days: ");
+        int n = sc.nextInt();
+        int maxProfit = 0;
+        
+        int[] prices = new int[n];
+        System.out.println("Enter the stock prices/day: ");
+        for (int index = 0; index <n; index++) {
+            prices[index] = sc.nextInt();
+        }
+        for(int i = 1;i<n;i++){
+            if(prices[i-1]>prices[i])
+                maxProfit+=prices[i]-prices[i-1];
+        }
+
+
+        System.out.println("The max profit that can be earned: "+maxProfit);
+        sc.close();
+
+    }
     
 }
