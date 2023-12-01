b8f32c2f19d742d7eeb87d53ac72d85382e4c990
Sachin <prajapatsachin7078@gmail.com>
2023-12-01 06:35:06 +0530

    9th Day Completed

---
+++ DSA_1/Matrix/TransposeOfMatrix.java
@@ -0,0 +1,52 @@
+package DSA_1.Matrix;
+
+import java.util.Scanner;
+
+public class TransposeOfMatrix {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();// as matrix should be square matrix
+        int[][] arr = new int[n][n];
+        
+         for(int i = 0;i<n;i++){
+            for(int j = 0;j<n;j++){
+                
+                arr[i][j]= sc.nextInt();
+            }
+        }
+        System.out.println("Before transpose:");
+         for(int i = 0;i<n;i++){
+            for(int j = 0;j<n;j++){
+                
+                System.out.print(arr[i][j] + " ");
+            }
+            System.out.println();
+        }
+        transpose(n,arr);
+        System.out.println("After transpose:");
+        for(int i = 0;i<n;i++){
+            for(int j = 0;j<n;j++){
+                
+                System.out.print(arr[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+    
+    public static void transpose(int n,int a[][])
+    {
+        for(int i = 0;i<n;i++){
+            for(int j = i+1;j<n;j++){
+                swap(a,i,j);
+            }
+        }
+    }
+    
+    public static void swap(int[][] array, int index1, int index2) {
+        // Swap the elements at the specified indices
+        int temp = array[index1][index2];
+        array[index1][index2] = array[index2][index1];
+        array[index2][index1] = temp;
+    }
+}
