1becdab66a2e1a213eac7ba24168815d1b635e71
Sachin <prajapatsachin7078@gmail.com>
2023-12-07 04:45:11 +0530

    change applied

--- DSA_1/Arrays/StockBuySellAllPossibleDays.java
+++ DSA_1/Arrays/StockBuySellAllPossibleDays.java
@@ -4,34 +4,35 @@ import java.util.*;
 
 public class StockBuySellAllPossibleDays {
     public static void main (String[] args) throws IOException {
-            Scanner sc  = new Scanner(System.in);
-            int n = sc.nextInt();
-            int A[] = new int[n];
-            
-            for(int i=0; i<n; i++){
-                A[i] = sc.nextInt();
+            try (Scanner sc = new Scanner(System.in)) {
+                int n = sc.nextInt();
+                int A[] = new int[n];
+                
+                for(int i=0; i<n; i++){
+                    A[i] = sc.nextInt();
+                }
+                int p = 0;
+                for(int i=0; i<n-1; i++)
+                    p += Math.max(0,A[i+1]-A[i]);
+                    
+                Solution obj = new Solution();
+                ArrayList<ArrayList<Integer> > ans = obj.stockBuySell(A, n);
+                if(ans.size()==0)
+                    System.out.print("No Profit");
+                else{
+                    int c=0;
+                    for(int i=0; i<ans.size(); i++)
+                        c += A[ans.get(i).get(1)]-A[ans.get(i).get(0)];
+                    
+                    if(c==p)
+                        System.out.print(1);
+                    else
+                        System.out.print(0);
+                }
             }
-            int p = 0;
-            for(int i=0; i<n-1; i++)
-                p += Math.max(0,A[i+1]-A[i]);
-                
-            Solution obj = new Solution();
-            ArrayList<ArrayList<Integer> > ans = obj.stockBuySell(A, n);
-            if(ans.size()==0)
-                System.out.print("No Profit");
-            else{
-                int c=0;
-                for(int i=0; i<ans.size(); i++)
-                    c += A[ans.get(i).get(1)]-A[ans.get(i).get(0)];
-                
-                if(c==p)
-                    System.out.print(1);
-                else
-                    System.out.print(0);
-            }System.out.println();
+            System.out.println();
     }
 }
-
 class Solution{
     //Function to find the days of buying and selling stock for max profit.
     ArrayList<ArrayList<Integer> > stockBuySell(int A[], int n) {
