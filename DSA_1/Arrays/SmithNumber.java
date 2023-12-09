9f21006e727a01da849ecd43e7f035ce30d9916c
Sachin <prajapatsachin7078@gmail.com>
2023-12-09 07:36:33 +0530

    15th dya completed

--- DSA_1/Arrays/SmithNumber.java
+++ DSA_1/Arrays/SmithNumber.java
@@ -1,82 +1,59 @@
 package DSA_1.Arrays;
-
-//{ Driver Code Starts
-//Initial Template for Java
-
-import java.io.*;
-import java.util.*;
+import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.List;
 
 public class SmithNumber {
-    public static void main(String args[]) throws IOException {
-        BufferedReader read =
-            new BufferedReader(new InputStreamReader(System.in));
-        int t = Integer.parseInt(read.readLine());
-        while (t-- > 0) {
-            int n = Integer.parseInt(read.readLine());
+    public static void main(String args[]) {
+        Scanner scanner = new Scanner(System.in);
 
-            Solution ob = new Solution();
-            System.out.println(ob.smithNum(n));
+        System.out.println("Enter the number of test cases:");
+        int t = scanner.nextInt();
+
+        while (t-- > 0) {
+            System.out.println("Enter the value of n:");
+            int n = scanner.nextInt();
+            scanner.nextLine();
+            Solution solution = new Solution();
+            System.out.println("Result for n=" + n + ": " + solution.smithNum(n));
+            // Consume the remaining EOL character
+            scanner.nextLine();
         }
+
+        // Close the scanner to avoid resource leak
+        scanner.close();
     }
 }
-// } Driver Code Ends
 
-
-//User function Template for Java
 class Solution {
-
-    // Function to check if a number is a Smith number
     public int smithNum(int n) {
         int sum = 0;
         int num = n;
-        List<Integer> primes = primeFactors(n);
-
-        // Calculate sum of digits of the given number
-        int digitSum = sumOfDigits(n);
-
-        // Calculate sum of digits of prime factors
-        int primeFactorSum = sumOfDigitsOfPrimes(primes);
-
-        // Check if the sums are equal
-        return digitSum == primeFactorSum ? 1 : 0;
-    }
-
-    // Function to find prime factors of a number
-    private List<Integer> primeFactors(int n) {
         List<Integer> primes = new ArrayList<>();
 
-        for (int i = 2; i <= n / 2; i++) {
-            while (n % i == 0) {
+        for (int i = 2; i < n; i++) {
+            while (num % i == 0) {
                 primes.add(i);
-                n /= i;
+                num /= i;
             }
         }
 
-        return primes;
-    }
-
-    // Function to calculate sum of digits of a number
-    private int sumOfDigits(int n) {
-        int sum = 0;
+        num = 0;
         while (n > 0) {
             int digit = n % 10;
-            sum += digit;
+            num += digit;
             n /= 10;
         }
-        return sum;
-    }
 
-    // Function to calculate sum of digits of prime factors
-    private int sumOfDigitsOfPrimes(List<Integer> primes) {
-        int sum = 0;
-        for (int prime : primes) {
+        for (int i = 0; i < primes.size(); i++) {
+            int prime = primes.get(i);
             while (prime > 0) {
                 int digit = prime % 10;
                 sum += digit;
                 prime /= 10;
             }
         }
-        return sum;
+
+        return num == sum ? 1 : 0;
     }
 }
-
