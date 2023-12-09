9f91a85549884d3a066bdeb7b8807f74324ff684
Sachin <prajapatsachin7078@gmail.com>
2023-12-09 07:28:47 +0530

    14th day completed

--- DSA_1/Arrays/SmithNumber.java
+++ DSA_1/Arrays/SmithNumber.java
@@ -24,34 +24,59 @@ public class SmithNumber {
 
 //User function Template for Java
 class Solution {
+
+    // Function to check if a number is a Smith number
     public int smithNum(int n) {
         int sum = 0;
         int num = n;
+        List<Integer> primes = primeFactors(n);
+
+        // Calculate sum of digits of the given number
+        int digitSum = sumOfDigits(n);
+
+        // Calculate sum of digits of prime factors
+        int primeFactorSum = sumOfDigitsOfPrimes(primes);
+
+        // Check if the sums are equal
+        return digitSum == primeFactorSum ? 1 : 0;
+    }
+
+    // Function to find prime factors of a number
+    private List<Integer> primeFactors(int n) {
         List<Integer> primes = new ArrayList<>();
 
-        for (int i = 2; i < n; i++) {
-            while (num % i == 0) {
+        for (int i = 2; i <= n / 2; i++) {
+            while (n % i == 0) {
                 primes.add(i);
-                num /= i;
+                n /= i;
             }
         }
 
-        num = 0;
+        return primes;
+    }
+
+    // Function to calculate sum of digits of a number
+    private int sumOfDigits(int n) {
+        int sum = 0;
         while (n > 0) {
             int digit = n % 10;
-            num += digit;
+            sum += digit;
             n /= 10;
         }
+        return sum;
+    }
 
-        for (int i = 0; i < primes.size(); i++) {
-            int prime = primes.get(i);
+    // Function to calculate sum of digits of prime factors
+    private int sumOfDigitsOfPrimes(List<Integer> primes) {
+        int sum = 0;
+        for (int prime : primes) {
             while (prime > 0) {
                 int digit = prime % 10;
                 sum += digit;
                 prime /= 10;
             }
         }
-
-        return num == sum ? 1 : 0;
+        return sum;
     }
 }
+
